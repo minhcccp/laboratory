e4ed85eec3aed13c7c52f9000eef9fc762b7f334
minhcccp <minhfacebook0802@gmail.com>
2020-09-02 11:11:24 +0700

    Trial #2

---
+++ pilot/axioms_alpha/palindrome_checker.py
@@ -0,0 +1,35 @@
+# From: https://en.wikipedia.org/wiki/Palindrome
+from typing import Union
+
+from sympy.ntheory import is_palindromic
+
+print(__file__, __name__, str(__package__))
+
+from . import FULL_INTEGER_CLASS, is_integer
+
+
+def is_palindrome(
+    input_data: Union[str, FULL_INTEGER_CLASS], number_base: FULL_INTEGER_CLASS = 0
+) -> bool:
+    """
+    :param input_data: Object to be checked, must be either a string or an integer
+    :param number_base: Default is 0 (i.e. the object is a str), value must be changed if the object is an integer
+    :return: Boolean result whether the object is palindromic
+    """
+
+    if isinstance(input_data, str) and number_base:
+        raise Exception("number_base must be 0 if input_data is a str")
+    elif is_integer(input_data) and number_base < 1:
+        raise Exception("number_base cannot be smaller than 1 if input_data is an int")
+
+    if number_base:
+        input_data: FULL_INTEGER_CLASS
+        return is_palindromic(input_data, number_base)
+    else:
+        input_data: str
+        return input_data == input_data[::-1]
+
+
+if __name__ == "__main__":
+    print(is_palindrome(1234321, 10))
+    print(is_palindrome("abcba"))
---
+++ pilot/lab_alpha/trial_1.py
@@ -0,0 +1,5 @@
+# From:
+from axioms_alpha.palindrome_checker import is_palindrome
+if __name__ == "__main__":
+    print(is_palindrome("my life sucks"))
+
