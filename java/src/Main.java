37f5cf0dbc0558724e0bd2a2026a7b821a589c5a
minhf <minhfacebook0802@gmail.com>
2020-12-11 16:45:29 +0000

    Reorganization #2

---
+++ java/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+  /**
+   * Iterate through each line of input.
+   */
+  public static void main(String[] args) throws IOException {
+    reverseSpell("And now          this:");
+  }
+
+  public static void reverseSpell(String input) {
+    String result = "";
+
+    for (int i = 0; i < input.length(); i++) {
+      char currentChar = Character.toLowerCase(input.charAt(i));
+      if (Character.isDigit(currentChar) || Character.isLetter(currentChar)) {
+        result = String.valueOf(currentChar) + (i > 0 ? "-" : "") + result;
+      }
+    }
+    System.out.println(result);
+  }
+}
